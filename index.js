af60998b06b0c34c1da58a1a97f24c24c253106d
ramachandrareddy <rcrmca1000@gmail.com>
2018-12-28 10:26:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let router= require('./routes/index');
 let config= require('./config/dev');
 let app = express();
 //ram testing purpose
+///jdjajjjjajsjdsjdjajsd
 app.use('/',router);
 app.listen(config.port,(error)=>{
     if(error) console.log('error' +error); else console.log(`server running on the port ${config.port}`);
